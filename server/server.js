86e094646456189c0d7600b41b7318d8061f2484
codingupstarts <codingmythz@gmail.com>
2021-08-27 13:46:31 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+// Set up the express app
+const app = express();
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
+
+module.exports = app;
+
+
+// var pgtools = require("pgtools");
+// const config = {
+//   user: "postgres",
+//   host: "localhost",
+//   password: "1234",
+//   port: 5432
+// };
+
+// pgtools.createdb(config, "some_db", function(err, res) {
+//   if (err) {
+//     console.error(err);
+//     process.exit(-1);
+//   }
+//   console.log(res);
+// });
